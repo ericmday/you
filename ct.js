88817c643763c9cb57e99544b0b5e1f167335a9b
ericmday <eric.michael.day@gmail.com>
2021-10-18 11:48:03 -0700

    Update ct.js

--- ct.js
+++ ct.js
@@ -1,6 +1,6 @@
   window.onload = function() {
         // alert('Page loaded');
     document.querySelector("a[href='/content']").remove();
-//     document.querySelector(".notion-navbar").remove();
+    document.querySelector(".notion-navbar").remove();
     document.querySelector("#page-content .notion-collection-table__wrapper").remove();
 };
